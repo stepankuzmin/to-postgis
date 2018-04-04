d7e238160738b87d9107db53f13ad3d9eb4ff443
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2018-04-04 14:44:16 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# to-postgis
+
+Loads files in different spatial formats into PostGIS using ogr2ogr.
+
+## Install
+
+```shell
+npm i -g to-postgis
+```
+
+## Usage
+
+```shell
+to-postgis input.geojson postgres://postgres@localhost:5432/db
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+
+const ogr2ogr = require('ogr2ogr');
+const ConnectionParameters = require('pg/lib/connection-parameters');
+
+const [fileName, connectionString] = process.argv.slice(2);
+
+if (!fileName || !connectionString) {
+  console.error('Usage: to-postgis [fileName] [connectionString]');
+  process.exit(-1);
+}
+
+const connectionParameters = new ConnectionParameters(connectionString);
+connectionParameters.getLibpqConnectionString((error, libpqConnectionString) => {
+  if (error) {
+    console.error(error);
+    process.exit(-1);
+  }
+
+  console.log(`Uploading ${fileName} to ${connectionString}`);
+
+  ogr2ogr(fileName)
+    .format('PostgreSQL')
+    .destination(`PG:${libpqConnectionString}`)
+    .project('EPSG:3857')
+    .exec((error2) => {
+      if (error2) {
+        console.error(error);
+        process.exit(-1);
+      }
+
+      console.log('Done!');
+    });
+});
